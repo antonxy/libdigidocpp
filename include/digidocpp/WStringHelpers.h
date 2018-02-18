6564c191b866cdd46b2e35c72146fdfd7977c72e
Anton Schirg <anton.schirg@posteo.de>
2018-02-18 19:39:46 +0100

    let WStringHelpers do nothing if no conversion is needed

--- include/digidocpp/WStringHelpers.h
+++ include/digidocpp/WStringHelpers.h
@@ -9,10 +9,20 @@ inline std::wstring s2ws(const std::string& str)
     return converterX.from_bytes(str);
 }
 
+inline std::wstring s2ws(const std::wstring& str)
+{
+    return str;
+}
+
 inline std::string ws2s(const std::wstring& wstr)
 {
     using convert_typeX = std::codecvt_utf8<wchar_t>;
     std::wstring_convert<convert_typeX, wchar_t> converterX;
 
     return converterX.to_bytes(wstr);
+}
+
+inline std::string ws2s(const std::string& str)
+{
+    return str;
 }
